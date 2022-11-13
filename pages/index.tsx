9deb3bb08195e8043f50ce578f44fa04a39f722a
zorro901 <zoro907@gmail.com>
2022-11-13 17:51:40 +0900

    Modify export

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
